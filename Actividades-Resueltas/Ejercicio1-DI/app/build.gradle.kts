5d10fb96d77a6dd1190de2801ab3c1df3d5e1e5f
JulenR14 <julen20191414@gmail.com>
2023-10-14 18:09:51 +0200

    Diseño terminado

--- Actividades-Resueltas/Ejercicio1-DI/app/build.gradle.kts
+++ Actividades-Resueltas/Ejercicio1-DI/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.ejercicio1_di"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ejercicio1_di"
--- Actividades-Resueltas/Ejercicio1-DI/app/src/main/java/com/example/ejercicio1_di/MainActivity.java
+++ Actividades-Resueltas/Ejercicio1-DI/app/src/main/java/com/example/ejercicio1_di/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ejercicio1_di;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.newplayer);
+        setContentView(R.layout.activity_main);
+
+        Button jugador = findViewById(R.id.button2);
+
+        jugador.setOnClickListener(v -> {
+
+            Intent i = new Intent(this, NewPlayer.class);
+            startActivity(i);
+        });
     }
 }
