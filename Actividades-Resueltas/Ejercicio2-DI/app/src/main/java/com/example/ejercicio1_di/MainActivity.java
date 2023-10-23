143988e62bd47dd5d831ffb3ef6c5584ea9881cb
JulenR14 <julen20191414@gmail.com>
2023-10-23 19:19:37 +0200

    Ejercicio Temas y estilos terminado

--- Actividades-Resueltas/Ejercicio2-DI/app/src/main/java/com/example/ejercicio1_di/MainActivity.java
+++ Actividades-Resueltas/Ejercicio2-DI/app/src/main/java/com/example/ejercicio1_di/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(this, NewPlayer.class);
             startActivity(i);
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_buscar){
+            Toast.makeText(this, "Busqueda", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
