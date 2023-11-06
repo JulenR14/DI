b9c47498fa068e37b7d92449886b584b3c6b4762
JulenR14 <julen20191414@gmail.com>
2023-11-06 20:01:39 +0100

    Ejercicio 4 apartado 1 empezado

--- Actividades-Resueltas/Ejercicio4-DI/app/src/main/java/com/example/ejercicio1_di/NewPlayer.java
+++ Actividades-Resueltas/Ejercicio4-DI/app/src/main/java/com/example/ejercicio1_di/NewPlayer.java
@@ -2,7 +2,13 @@ package com.example.ejercicio1_di;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NewPlayer extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class NewPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newplayer);
+
+        String[] telefonos = {"Telefono 1", "Telefono 2", "Telefono 3", "Telefono 4", "Telefono 5", "Telefono 6", "Telefono 7", "Telefono 8", "Telefono 9", "Telefono 10"};
+
+        Spinner spinner = findViewById(R.id.spinner);
+        EditText texto = findViewById(R.id.phone);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, telefonos);
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adaptador);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
