4e150a54d3c82a2ed3275eea8f51ae51aaeeb192
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-12-08 08:52:55 +0100

    He hecho pruebas con el theme

--- Actividades-Resueltas/PruebaExamen1_DI/app/src/main/java/com/example/pruebaexamen1_di/MainActivity.java
+++ Actividades-Resueltas/PruebaExamen1_DI/app/src/main/java/com/example/pruebaexamen1_di/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Base_Theme_temaSiActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
