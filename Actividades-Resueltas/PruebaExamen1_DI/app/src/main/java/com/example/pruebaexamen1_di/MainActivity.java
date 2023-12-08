6246bc0b1e5d2791f526f300eb56aef836a9dacc
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-12-08 16:18:29 +0100

    nada importante

--- Actividades-Resueltas/PruebaExamen1_DI/app/src/main/java/com/example/pruebaexamen1_di/MainActivity.java
+++ Actividades-Resueltas/PruebaExamen1_DI/app/src/main/java/com/example/pruebaexamen1_di/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fabgo = findViewById(R.id.fabgo);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
 
         CardAdapter adaptador = new CardAdapter(datos);
 
